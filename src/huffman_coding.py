62753e94f0a71b203e342a9dc0a50c2d890e1ce0
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-01-28 14:31:49 +0200

    Fixed pylint errors

--- src/huffman_coding.py
+++ src/huffman_coding.py
@@ -1,11 +1,14 @@
+""" File compressing and decompressing algorithm"""
+
 class HuffmanCoding:
-    """File compressing algorithm"""
+    """File compressing and decompressing algorithm"""
     def __init__(self):
         self.frequence_table = {}
-    
+
     def get_string_from_file(self, path):
-        """Gets a path to a file as a parameter and returns a string of the file content"""
-        with open(path, "r") as file:
+        """Gets a path to a file as a parameter and returns
+        a string of the file content"""
+        with open(path, "r", encoding="utf-8") as file:
             string = file.read()
             string = string.strip()
 
@@ -20,14 +23,13 @@ class HuffmanCoding:
         return self.frequence_table
 
     def create_minimum_heap(self, string):
-        """Creates a minimum heap of the charcters in the string according to their frequence."""
-        pass
+        """Creates a minimum heap of the charcters
+        in the string according to their frequence."""
 
     def create_huffman_tree(self):
-        """Creates a Huffman tree by always taking two minimum nodes from the minimum heap and merges them."""
-        pass
+        """Creates a Huffman tree by always taking
+        two minimum nodes from the minimum heap and merges them."""
 
     def encode(self):
-        """Assigns codes, 0's and 1's, to the nodes in the Huffman tree. 
+        """Assigns codes, 0's and 1's, to the nodes in the Huffman tree.
         The codes are then used to write a compressed file."""
-        pass
--- src/lz77.py
+++ src/lz77.py
@@ -1,19 +1,25 @@
+"""
+    File compressing and decompressing algorithm
+"""
 class LZ77:
-    """File compressing algorithm"""
-    
+    """
+    File compressing and decompressing algorithm
+    """
     def __init__(self):
-        """A search window is the list of already seen characters from 
+        """A search window is the list of already seen characters from
         where we are looking for matches for the the characters
-        in the look-ahead window. The integers assigned for the variables are 
-        lengths of the windows in bits. These can be changed later, the fixed lengths are just for 
+        in the look-ahead window. The integers assigned for the variables are
+        lengths of the windows in bits. These can be changed later,
+        the fixed lengths are just for
         the start of the development project."""
         self.search_window = 1024
         self.lookahead_window = 16
 
 
     def get_string_from_file(self, path):
-        """Gets a path to a file as a parameter and returns a string of the file content"""
-        with open(path, "r") as file:
+        """Gets a path to a file as a parameter and
+        returns a string of the file content"""
+        with open(path, "r", encoding="utf-8") as file:
             string = file.read()
             string = string.strip()
 
@@ -21,14 +27,14 @@ class LZ77:
 
     def search_longest_match(self):
         """Finds the longest match in the search_window for
-        the next character(s) in the lookahead_window. Returns a triplet (distance, length, character). 
-        d = The distance to the start of the match, 
+        the next character(s) in the lookahead_window.
+        Returns a triplet (distance, length, character).
+        d = The distance to the start of the match,
         l = the lenght of the match,
-        c = the next character in the lookahead table that was not matched in the search windows."""
-        pass
+        c = the next character in the lookahead table that was not
+        matched in the search windows."""
 
     def compress(self):
-        """Creates a compressed file. Matching charcter(s) is written 
-        in the compressed file in the form of information about 
+        """Creates a compressed file. Matching charcter(s) is written
+        in the compressed file in the form of information about
         the distance to the start of the match and the lenght of the match"""
-        pass
