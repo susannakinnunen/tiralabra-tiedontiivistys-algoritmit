2a2519e75053c863f5c119b1606949fdeea5f69a
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-02-08 10:39:31 +0200

    Created a preliminary ui

---
+++ src/ui_huffman.py
@@ -0,0 +1,12 @@
+from huffman_coding import HuffmanCoding
+
+class UI:
+    def __init__(self, compressor):
+        self.compressor = compressor
+        self.compressor.compress()
+
+if __name__ == "__main__":
+    text_file_path = input("Anna tekstitiedoston polku:")
+    compressor = HuffmanCoding(text_file_path, "compress")
+    UI(compressor)
+    print("Tiedosto on kompressoitu samaan kansioon kuin missä olet")
