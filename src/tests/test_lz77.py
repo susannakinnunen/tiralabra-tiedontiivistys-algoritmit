49da2fb50f415c1815724190bb7ce4b78b684715
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-02-19 16:44:05 +0200

    Create test for match searcher

--- src/tests/test_lz77.py
+++ src/tests/test_lz77.py
@@ -3,20 +3,22 @@ import os
 from lz77 import LZ77
 
 with open(os.path.join(os.getcwd(), "test_lz77.txt"), "w") as test_file:
-        test_file.write("Test text: \n12368")
+        test_file.write("ABBAABBABB")
 
 class TestLZ77Coding(unittest.TestCase):
     def setUp(self):
         self.path = os.path.join(os.getcwd(), "test_lz77.txt")
-        self.lz77 = LZ77(self.path)
+        self.lz77 = LZ77(self.path, True)
 
     def test_get_string_from_file(self):
         """Test get_string_from_file method"""     
         string = self.lz77.get_string_from_file(self.path)
 
-        self.assertEqual(string, "Test text: \n12368")
+        self.assertEqual(string, "ABBAABBABB")
 
-    
+    def test_compress_and_search_longest_match(self):
+        compressed_info_list = self.lz77.compress()
 
+        self.assertEqual(compressed_info_list, [(0, 1,"A"), (0, 1,"B"), (0, 1,"B"), (0, 1,"A"), (4, 4, 0), (3, 2, 0)])
 
 
