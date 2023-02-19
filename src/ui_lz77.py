7f3891e2174a42817adbbad019523d51d533cb59
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-02-19 20:19:32 +0200

    preliminary ui for lz77

---
+++ src/ui_lz77.py
@@ -0,0 +1,21 @@
+"""This is a preliminary user interface for file compression and decompression."""
+from lz77 import LZ77
+import os
+
+class UI:
+    def __init__(self, text_file_path):
+        self.text_file_path = text_file_path
+        self.compress()
+
+    def compress(self):
+        return LZ77(self.text_file_path)
+
+
+if __name__ == "__main__":
+    print("Tämä on LZ77 algoritmiin perustuva tiedonpakkausohjelma.")
+    text_file_path = input("Anna tekstitiedoston polku:")
+    ui = UI(text_file_path)
+    compressed_file_path = os.path.join(
+            os.getcwd(), "lz77_compressed.bin")
+    print("\n")
+    print(f"Kompressoitu tiedosto löytyy polusta {compressed_file_path}.")
