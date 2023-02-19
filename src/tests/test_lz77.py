c4354a4904495aa4aa9f8340b0abd39c7f02abe2
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-02-19 16:22:02 +0200

    minor fix in the test

--- src/tests/test_lz77.py
+++ src/tests/test_lz77.py
@@ -2,18 +2,17 @@ import unittest
 import os
 from lz77 import LZ77
 
+with open(os.path.join(os.getcwd(), "test_lz77.txt"), "w") as test_file:
+        test_file.write("Test text: \n12368")
+
 class TestLZ77Coding(unittest.TestCase):
     def setUp(self):
-        self.lz77 = LZ77()
+        self.path = os.path.join(os.getcwd(), "test_lz77.txt")
+        self.lz77 = LZ77(self.path)
 
     def test_get_string_from_file(self):
-        """Test get_string_from_file method"""
-        with open(os.path.join(os.getcwd(), "test_lz77.txt"), "w") as test_file:
-            test_file.write("Test text: \n12368")
-        
-        path = os.path.join(os.getcwd(), "test_lz77.txt")
-        
-        string = self.lz77.get_string_from_file(path)
+        """Test get_string_from_file method"""     
+        string = self.lz77.get_string_from_file(self.path)
 
         self.assertEqual(string, "Test text: \n12368")
 
