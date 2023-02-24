1315c5fc672479eee31e3823b4bce3071251139f
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-02-24 14:07:56 +0200

    Create tests with small and big files

---
+++ src/tests/test_lz77_kalevala.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+from lz77 import LZ77
+
+
+class TestLZ77Coding(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(os.getcwd(), "kalevala.txt")
+        self.lz77 = LZ77(self.path)
+
+    def test_compress_is_smaller(self):
+        """Tests if the compressed file size is at least 60 % smaller than the original file size"""
+        original_file_size = os.path.getsize(os.path.join(os.getcwd(), "kalevala.txt"))
+        compressed_file_size = os.path.getsize(os.path.join(os.getcwd(), "lz77_compressed.bin"))
+
+        assert compressed_file_size/original_file_size <= 0.61
+
+    def test_decompressed_is_same_as_original(self):
+        with open("kalevala.txt", "r", encoding="utf-8")as original_file, open("lz77_decompressed.txt", "r", encoding="utf-8")as decompressed_file:
+            original_string = original_file.read()
+            decompressed_string = decompressed_file.read()
+
+        assert original_string == decompressed_string
---
+++ src/tests/test_lz77_small.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+from lz77 import LZ77
+
+with open(os.path.join(os.getcwd(), "test_lz77.txt"), "w") as test_file:
+        test_file.write("ABBAABBABB")
+
+class TestLZ77Coding(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join(os.getcwd(), "test_lz77.txt")
+        self.lz77 = LZ77(self.path, True)
+        self.compressed_info_list = self.lz77.compress()
+
+    def test_get_string_from_file(self):
+        """Test get_string_from_file method"""     
+        string = self.lz77.get_string_from_file(self.path)
+
+        self.assertEqual(string, "ABBAABBABB")
+
+    def test_compress_and_search_longest_match(self):
+        self.assertEqual(self.compressed_info_list, [(0, 1,"A"), (0, 1,"B"), (0, 1,"B"), (0, 1,"A"), (4, 4, 0), (3, 2, 0)])
+
+
+    def test_compress_and_convert_into_tuples_to_bit_string(self):
+        bit_string = self.lz77.convert_into_bit_string(self.compressed_info_list)
+
+        self.assertEqual(bit_string, "01000001010000100100001001000001100000000010001000000000100000000001100100000000")
+
+    
+    def test_convert_into_bytes(self):
+        bit_string = self.lz77.convert_into_bit_string(self.compressed_info_list)
+        byte_list = self.lz77.convert_into_bytes(bit_string)
+
+        self.assertEqual(bit_string, "01000001010000100100001001000001100000000010001000000000100000000001100100000000")
+
+        self.assertEqual(str(byte_list), """bytearray(b'ABBA\\x80"\\x00\\x80\\x19\\x00')""" )
+
+
