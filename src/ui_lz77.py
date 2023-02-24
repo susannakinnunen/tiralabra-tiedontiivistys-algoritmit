7d42c1c06fdf7c875c7b80d5a11082526f6b4837
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-02-24 14:06:35 +0200

    Removed unnecessary parts

--- src/ui_lz77.py
+++ src/ui_lz77.py
@@ -5,17 +5,18 @@ import os
 class UI:
     def __init__(self, text_file_path):
         self.text_file_path = text_file_path
-        self.compress()
+        LZ77(self.text_file_path)
 
-    def compress(self):
-        return LZ77(self.text_file_path)
+
 
 
 if __name__ == "__main__":
+    print("\n")
     print("Tämä on LZ77 algoritmiin perustuva tiedonpakkausohjelma.")
     text_file_path = input("Anna tekstitiedoston polku:")
     ui = UI(text_file_path)
     compressed_file_path = os.path.join(
             os.getcwd(), "lz77_compressed.bin")
+    decompressed_file_path = os.path.join(os.getcwd(), "lz77_decompressed.txt")
     print("\n")
-    print(f"Kompressoitu tiedosto löytyy polusta {compressed_file_path}.")
+    print(f"Kompressoitu tiedosto löytyy polusta {compressed_file_path} ja dekompressoitu polusta {decompressed_file_path}")
